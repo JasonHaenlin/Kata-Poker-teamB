bf08de563f784c14cddcf22dbeeed40fff86261d
Jason Haenlin <jasonjas5252@gmail.com>
2018-09-26 15:29:26 +0200

    sortedSet to ArrayList + tests de la carte la plus haute

--- src/test/java/poker/WinnerTest.java
+++ src/test/java/poker/WinnerTest.java
@@ -5,12 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class WinnerTest {
 
-	@Ignore
 	@Test
 	public void winnerTest() {
 		Referee referee = new Referee();
@@ -19,12 +17,12 @@ public class WinnerTest {
 		Hand hand3 = new Hand(1);
 		Hand hand4 = new Hand(2);
 
-		hand1.buildNewHand(new ArrayList<>(Arrays.asList("7")));
-		hand2.buildNewHand(new ArrayList<>(Arrays.asList("14")));
-		assertEquals(2, referee.winner(hand1, hand2)); // victoire de la main1
+		hand1.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "7", "9", "10")));
+		hand2.buildNewHand(new ArrayList<>(Arrays.asList("14", "8", "2", "14", "12")));
+		assertEquals(2, referee.winner(hand1, hand2)); // victoire de la main2
 
-		hand3.buildNewHand(new ArrayList<>(Arrays.asList("12")));
-		hand4.buildNewHand(new ArrayList<>(Arrays.asList("3")));
+		hand3.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "14", "9", "9")));
+		hand4.buildNewHand(new ArrayList<>(Arrays.asList("9", "8", "2", "8", "12")));
 		assertEquals(1, referee.winner(hand3, hand4)); // victoire de la main2
 
 		assertEquals(0, referee.winner(hand1, hand1)); // égalité

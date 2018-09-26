1b1489923827f3e9d4890c95560269d270bdd9cc
Jason Haenlin <jasonjas5252@gmail.com>
2018-09-26 23:07:48 +0200

    arrange le merge. Attention, carte haute broken pour le moment

--- src/test/java/poker/RefereeTest.java
+++ src/test/java/poker/RefereeTest.java
@@ -1,43 +1,14 @@
 package poker;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 public class RefereeTest {
-
-    @Test
-    public void winnerTest() {
-        Referee referee = new Referee();
-        Hand hand1 = new Hand(1);
-        Hand hand2 = new Hand(2);
-        Hand hand3 = new Hand(1);
-        Hand hand4 = new Hand(2);
-
-        hand1.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "7", "9", "10")));
-        hand2.buildNewHand(new ArrayList<>(Arrays.asList("14", "8", "2", "11", "12")));
-        assertEquals(1, referee.winner(hand1, hand2));   //La main 1 doit gagner avec une paire
-
-        assertEquals(2, referee.winner(hand2, hand1));
-
-        hand3.buildNewHand(new ArrayList<>(Arrays.asList("14", "8", "2", "14", "12")));
-        assertEquals(0, referee.winner(hand1, hand3));//Les deux mains ont une paire
-        assertEquals(0, referee.winner(hand1, hand1));
-        assertEquals(0, referee.winner(hand2, hand2));
-
-        hand4.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "7", "9", "7")));
-        assertEquals(1, referee.winner(hand4, hand2));//La main 4 gagne avec un brelan face à rien
-        assertEquals(2, referee.winner(hand2, hand4));
-
-        assertEquals(1, referee.winner(hand4, hand1));//La main 4 gagne sur une paire
-        assertEquals(2, referee.winner(hand1, hand4));
-
-        assertEquals(0, referee.winner(hand4, hand4));
-    }
 
     Hand hand1, hand2, hand3, hand4, hand5;
     Referee referee;
@@ -51,6 +22,33 @@ public class RefereeTest {
         hand3 = new Hand(1);
         hand4 = new Hand(1);
         hand5 = new Hand(1);
+    }
+
+    @Test
+    public void winnerTest() {
+
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "7", "9", "10")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("14", "8", "2", "11", "12")));
+        //La main 1 doit gagner avec une paire
+        assertEquals(1, referee.winner(hand1, hand2));
+
+        assertEquals(2, referee.winner(hand2, hand1));
+
+        hand3.buildNewHand(new ArrayList<>(Arrays.asList("14", "8", "2", "14", "12")));
+        //Les deux mains ont une paire
+        assertEquals(0, referee.winner(hand1, hand3));
+        assertEquals(0, referee.winner(hand1, hand1));
+        assertEquals(0, referee.winner(hand2, hand2));
+
+        hand4.buildNewHand(new ArrayList<>(Arrays.asList("7", "4", "7", "9", "7")));
+        //La main 4 gagne avec un brelan face à rien
+        assertEquals(1, referee.winner(hand4, hand2));
+        assertEquals(2, referee.winner(hand2, hand4));
+        //La main 4 gagne sur une paire
+        assertEquals(1, referee.winner(hand4, hand1));
+        assertEquals(2, referee.winner(hand1, hand4));
+
+        assertEquals(0, referee.winner(hand4, hand4));
     }
 
     @Test
@@ -108,10 +106,6 @@ public class RefereeTest {
         // just Two pair
         hand5.buildNewHand(new ArrayList<>(Arrays.asList("7", "7", "12", "8", "8")));
         assertEquals(-1, referee.findFourOfAKind(hand5));
-
-
-        assertEquals(-1, referee.findFourOfAKind(hand5));
-
     }
 
 }

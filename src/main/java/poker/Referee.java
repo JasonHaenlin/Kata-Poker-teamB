298469148c636b43e605bd0c42b4678561c9f1a3
maxcastellano <maxime.castellano@etu.univ-cotedazur.fr>
2018-09-26 16:41:27 +0200

    Mise en place des methodes pour dÃterminer les gagnants hors cas d'Ã©gaitÃ© cas carte haute, pair, brelan et carrÃe)

--- src/main/java/poker/Referee.java
+++ src/main/java/poker/Referee.java
@@ -9,19 +9,44 @@ class Referee {
 	 * @param hand2
 	 * @return an int corresponding to the winner's handnumber
 	 */
-	int winner(Hand hand1, Hand hand2) {
-		int value1 = hand1.handCardHightestValue();
-		int value2 = hand2.handCardHightestValue();
 
-		if (value1 < value2) {
-			System.out.println("La main 2 gagne avec la carte la plus élevée : " + value2);
-			return 2;
-		} else if (value1 > value2) {
-			System.out.println("La main 1 gagne avec la carte la plus élevée : " + value1);
-			return 1;
-		} else {
-			System.out.println("Egalité !");
-			return 0;
-		}
+	String typeHand(Hand hand)  //Detecte le type de la main entre Carree, Brelan, Pair et carte haute
+    {
+        if(findSquare(hand) != -1)
+        {
+            return "Carre";
+        }
+        else if(findTriple(hand) != -1)
+        {
+            return "Brelan";
+        }
+        else if(findPair(hand) != -1)
+        {
+            return "Paire";
+        }
+        else {return "Carte la plus haute";}
+    }
+
+	int winner(Hand hand1, Hand hand2) {
+
+	    CombinationType combinaison1 = new CombinationType(typeHand(hand1));
+        CombinationType combinaison2 = new CombinationType(typeHand(hand2));
+
+	    int h1 = CombinationType.getType(hand1).ordinal(); //Récupère l'ordinal du type de la main 1
+        int h2 = CombinationType.getType(hand2).ordinal(); //Récupère l'ordinal du type de la main 2
+
+        if (h1 < h2){
+            System.out.println("La main 2 gagne ");
+            return 2;
+        }
+        else if(h2 < h1){
+            System.out.println("La main 1 gagne ");
+            return 1;
+        }
+        else
+        {
+            System.out.println("Les deux mains sont du même type");
+            return 0;
+        }
 	}
 }

b64e44700ca5424865f22129c34354910b63f71b
Jason Haenlin <jasonjas5252@gmail.com>
2018-10-08 19:13:23 +0200

    ajoute des testes pour les coef (revoir la suite)

--- src/test/java/poker/game/RefereeTest.java
+++ src/test/java/poker/game/RefereeTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RefereeTest {
@@ -29,10 +30,10 @@ public class RefereeTest {
     }
 
     @Test
-    public void duplicatedCardsBetweenHands(){
+    public void duplicatedCardsBetweenHands() {
         // cards are unique... don't copy the other hand
         try {
-            this.hand1.buildNewHand(new ArrayList<>(Arrays.asList("7Co","3Ca","7Pi","4Co","9Tr")));
+            this.hand1.buildNewHand(new ArrayList<>(Arrays.asList("7Co", "3Ca", "7Pi", "4Co", "9Tr")));
             this.hand2.buildNewHand(new ArrayList<>(Arrays.asList("10Tr", "VTr", "DTr", "RTr", "9Tr")));
 
             referee.checkForDuplicateCards(hand1, hand2);
@@ -151,7 +152,7 @@ public class RefereeTest {
         hand5.buildNewHand(new ArrayList<>(Arrays.asList("6Co", "2Co", "3Co", "4Co", "5Co")));
         hand6.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "ACo", "ACa", "9Co", "APi")));
 
-        assertEquals(5, referee.establishTheWinner(hand5,hand6));
+        assertEquals(5, referee.establishTheWinner(hand5, hand6));
 
     }
 
@@ -189,5 +190,71 @@ public class RefereeTest {
         hand6.buildNewHand(new ArrayList<>(Arrays.asList("ACa", "2Tr", "3Tr", "4Pi", "5Tr")));
 
         assertEquals(0, referee.establishTheWinner(hand5, hand6));
+    }
+
+    @Test
+    public void tresholdCoefScoreHighToPairTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("9Tr", "VTr", "DCa", "RCo", "ATr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Tr", "2Ca", "3Ca", "4Ca", "5Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Test
+    public void tresholdCoefScorePairToTwoPairTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "ACo", "RTr", "DTr", "VTr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Ca", "2Co", "3Ca", "3Co", "4Tr")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Test
+    public void tresholdCoefScoreTwoPairToThreeOfAKindTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "ACo", "RTr", "RCo", "DTr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Ca", "2Co", "2Tr", "3Ca", "4Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Ignore
+    @Test
+    public void tresholdCoefScoreThreeOfAKindToStraightTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "ACo", "ACa", "RTr", "VTr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("APi", "2Ca", "3Ca", "4Co", "5Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Ignore
+    @Test
+    public void tresholdCoefScoreStraightToColorTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("10Pi", "VCo", "DCa", "RTr", "ATr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Ca", "3Ca", "4Ca", "5Ca", "7Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Test
+    public void tresholdCoefScoreColorToFullTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("9Pi", "VPi", "DPi", "RPi", "APi")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Ca", "2Co", "2Pi", "3Ca", "3Co")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Test
+    public void tresholdCoefScoreFullToFourOfAKindTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "ACa", "ACo", "RTr", "RPi")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("2Ca", "2Tr", "2Co", "2Pi", "3Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
+    }
+
+    @Test
+    public void tresholdCoefScoreFourOfAKindToQuinteFlushTest() {
+        hand1.buildNewHand(new ArrayList<>(Arrays.asList("ATr", "APi", "ACo", "ACa", "RTr")));
+        hand2.buildNewHand(new ArrayList<>(Arrays.asList("6Ca", "2Ca", "3Ca", "4Ca", "5Ca")));
+
+        assertEquals(2, referee.establishTheWinner(hand1, hand2));
     }
 }
